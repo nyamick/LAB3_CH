188ee9ffee13cd8f8549c6e5fe1dbf9c7c96708e
nyamick <user@DESKTOP-LTBDATA>
2022-10-24 21:31:12 +0800

    Types

--- LAB3_CH/DIstance.cs
+++ LAB3_CH/DIstance.cs
@@ -6,7 +6,36 @@ using System.Threading.Tasks;
 
 namespace LAB3_CH
 {
+    public enum MeasureType { Ms, KMh, Uz, Max };
     public class DIstance
     {
+        private double value;
+        private MeasureType type;
+
+        public DIstance(double value, MeasureType type) 
+        {
+            this.value = value;
+            this.type = type;
+        }
+        public string Verbose()
+        {
+            string typeVerbose = "";
+            switch (this.type)
+            {
+                case MeasureType.Ms:
+                    typeVerbose = "м/с";
+                    break;
+                case MeasureType.KMh:
+                    typeVerbose = "км/ч";
+                    break;
+                case MeasureType.Uz:
+                    typeVerbose = "узлов";
+                    break;
+                case MeasureType.Max:
+                    typeVerbose = "Max";
+                    break;
+            }
+            return String.Format("{0} {1}", this.value, typeVerbose);
+        }
     }
 }
---
+++ LAB3_CHTests/DIstanceTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LAB3_CH;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LAB3_CH.Tests
+{
+    [TestClass()]
+    public class DIstanceTests
+    {
+
+
+        [TestMethod()]
+        public void VerboseTest()
+        {
+            // тестируем все четыре типа
+            var distance = new DIstance(38, MeasureType.Ms);
+            Assert.AreEqual("38 м/с", distance.Verbose());
+
+            distance = new DIstance(38, MeasureType.KMh);
+            Assert.AreEqual("38 км/ч", distance.Verbose());
+
+            distance = new DIstance(38, MeasureType.Uz);
+            Assert.AreEqual("38 узлов", distance.Verbose());
+
+            distance = new DIstance(38, MeasureType.Max);
+            Assert.AreEqual("38 Max", distance.Verbose());
+        }
+
+    }
+}
