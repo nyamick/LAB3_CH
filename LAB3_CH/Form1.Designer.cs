ee52ad9e8f5f6086fae1c85c99292d8eaad4b9a6
nyamick <user@DESKTOP-LTBDATA>
2022-10-29 19:43:46 +0800

    FormConstr

--- LAB3_CH/Form1.Designer.cs
+++ LAB3_CH/Form1.Designer.cs
@@ -28,12 +28,63 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.cmdOperation = new System.Windows.Forms.ComboBox();
+            this.txtFirst = new System.Windows.Forms.TextBox();
+            this.txtSecond = new System.Windows.Forms.TextBox();
+            this.txtResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmdOperation
+            // 
+            this.cmdOperation.FormattingEnabled = true;
+            this.cmdOperation.Location = new System.Drawing.Point(142, 62);
+            this.cmdOperation.Name = "cmdOperation";
+            this.cmdOperation.Size = new System.Drawing.Size(53, 28);
+            this.cmdOperation.TabIndex = 0;
+            // 
+            // txtFirst
+            // 
+            this.txtFirst.Location = new System.Drawing.Point(292, 63);
+            this.txtFirst.Name = "txtFirst";
+            this.txtFirst.Size = new System.Drawing.Size(125, 27);
+            this.txtFirst.TabIndex = 1;
+            this.txtFirst.TextChanged += new System.EventHandler(this.txtFirst_TextChanged);
+            // 
+            // txtSecond
+            // 
+            this.txtSecond.Location = new System.Drawing.Point(292, 96);
+            this.txtSecond.Name = "txtSecond";
+            this.txtSecond.Size = new System.Drawing.Size(125, 27);
+            this.txtSecond.TabIndex = 2;
+            // 
+            // txtResult
+            // 
+            this.txtResult.Location = new System.Drawing.Point(142, 96);
+            this.txtResult.Name = "txtResult";
+            this.txtResult.Size = new System.Drawing.Size(125, 27);
+            this.txtResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(560, 187);
+            this.Controls.Add(this.txtResult);
+            this.Controls.Add(this.txtSecond);
+            this.Controls.Add(this.txtFirst);
+            this.Controls.Add(this.cmdOperation);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private ComboBox cmdOperation;
+        private TextBox txtFirst;
+        private TextBox txtSecond;
+        private TextBox txtResult;
     }
 }
--- LAB3_CH/Form1.cs
+++ LAB3_CH/Form1.cs
@@ -6,5 +6,28 @@ namespace LAB3_CH
         {
             InitializeComponent();
         }
+
+        private void txtFirst_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                
+                var firstValue = double.Parse(txtFirst.Text);
+                var secondValue = double.Parse(txtSecond.Text);
+ 
+                var firstDistance = new DIstance(firstValue, MeasureType.Ms);
+                var secondDistance = new DIstance(secondValue, MeasureType.Ms);
+
+                
+                var sumLength = firstDistance + secondDistance;
+
+                
+                txtResult.Text = sumLength.Verbose();
+            }
+            catch (FormatException)
+            {
+                
+            }
+        }
     }
 }
